d2f626150af96c75c8aa9403345dbd7b954f68ef
shailesh097 <sailesh.pokharel.234@gmail.com>
2026-04-30 14:45:37 +0200

    feat: log file creation

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -1,6 +1,12 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"time"
+)
 
 const (
 	reset      = "\033[0m"
@@ -15,32 +21,68 @@ const (
 	lightGreen = "\033[92m"
 )
 
-type Logger struct{}
+type Logger struct {
+	termOut io.Writer // colored op to terminal
+	termErr io.Writer // colored err t terminal stderr
+	fileOut io.Writer // normal op to log file
+}
 
-func New() *Logger {
-	return &Logger{}
+func New() (*Logger, func(), error) {
+	logFile, err := createLogFile()
+	if err != nil {
+		return nil, nil, fmt.Errorf("Failed to create log file: %w", err)
+	}
+
+	closefile := func() { logFile.Close() }
+
+	return &Logger{
+		termOut: os.Stdout,
+		termErr: os.Stderr,
+		fileOut: logFile,
+	}, closefile, nil
+}
+
+func createLogFile() (*os.File, error) {
+	//get home dir
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	dir := filepath.Join(home, "personal_logs", "fedora_tweed_go")
+
+	// create dirs
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return nil, err
+	}
+
+	timestamp := time.Now().Format("2006-01-02_15_03_04")
+	filename := fmt.Sprintf("setup_%s.log", timestamp)
+	fullPath := filepath.Join(dir, filename)
+	return os.Create(fullPath) // create file in personal log dir
+}
+
+func (l *Logger) writeBoth(termWriter io.Writer, color, prefix, msg string) {
+	fmt.Fprintf(termWriter, "%s%s %s%s\n", color, prefix, msg, reset)
+	fmt.Fprintf(l.fileOut, "%s %s\n", prefix, msg)
 }
 
 // cyan for Information message
 func (l *Logger) Info(format string, args ...any) {
-	msg := fmt.Sprintf(format, args...)
-	fmt.Printf("%s%s==> %s%s\n", cyan, bold, msg, reset)
+	l.writeBoth(l.termOut, cyan, "==>", fmt.Sprintf(format, args...))
 }
 
 // yellow for Warning
 func (l *Logger) Warn(format string, args ...any) {
-	msg := fmt.Sprintf(format, args...)
-	fmt.Printf("%s %s%s\n", yellow, msg, reset)
+	l.writeBoth(l.termOut, yellow, "", fmt.Sprintf(format, args...))
 }
 
 // red for error
 func (l *Logger) Error(format string, args ...any) {
-	msg := fmt.Sprintf(format, args...)
-	fmt.Printf("%s %s%s\n", red, msg, reset)
+	l.writeBoth(l.termErr, red, "", fmt.Sprintf(format, args...))
 }
 
 // greeen for messages
 func (l *Logger) Message(format string, args ...any) {
-	msg := fmt.Sprintf(format, args...)
-	fmt.Printf("%s %s%s\n", green, msg, reset)
+	l.writeBoth(l.termOut, green, "", fmt.Sprintf(format, args...))
 }
